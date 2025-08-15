9880fdb8f3d99b790faeca4009201ef08a287284
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-15 14:40:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var config = {
     title: 'İstanbul Ataşehir Finans Merkezi / Ataşehir Finance Center',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     chapters: [
         {
             id: '0',
@@ -41,7 +41,7 @@ var config = {
                 //curve: 0, // change the speed at which it zooms out
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: true,
+            rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -51,7 +51,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '',
-            image: 'https://i.ibb.co/dJ0wXwQX/1.png',
+            image: 'https://i.ibb.co/bRLYvgyc/1.png',
             description: '',
             location: {
                center: [29.106880327805428,41.002030942693075],
@@ -84,7 +84,7 @@ var config = {
              image: 'https://i.ibb.co/Kxytr1Cw/2.png',
             description: '',
             location: {
-              center: [29.106880327805428,41.002030942693075],
+              center: [29.11748824257017,41.00127474175267],
                 zoom: 15,
                 pitch: 40,
                 bearing: 0
@@ -150,7 +150,7 @@ var config = {
                 bearing: 0
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: true,
+            rotateAnimation: false,
             callback: '',
             onChapterEnter: [
                 // {
@@ -187,7 +187,7 @@ var config = {
                 //curve: 0, // change the speed at which it zooms out
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: true,
+            rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -202,33 +202,7 @@ var config = {
             location: {
                 center: [29.1087514496626,41.00146109221629],
                 zoom: 15,
-                pitch: 10,
-                bearing: 0,
-              
-                // flyTo additional controls-
-                // These options control the flight curve, making it move
-                // slowly and zoom out almost completely before starting
-                // to pan.
-                //speed: 2, // make the flying slow
-                //curve: 0, // change the speed at which it zooms out
-            },
-            mapAnimation: 'right',
-            rotateAnimation: true,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-         {
-            id: '7',
-            alignment: 'full',
-            hidden: false,
-            title: '',
-            image: '',
-            description: '',
-            location: {
-                center: [29.106880327805428,41.002030942693075],
-                zoom: 12,
-                pitch: 10,
+                pitch: 0,
                 bearing: 0,
               
                 // flyTo additional controls-
@@ -239,10 +213,11 @@ var config = {
                 //curve: 0, // change the speed at which it zooms out
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: true,
+            rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
         },
+       
     ]
 };
